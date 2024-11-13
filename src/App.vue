40ccf6ad6517941b8bdc217fd75ede3cc93157b3
jhayco <manolojose36@hotmail.com>
2024-11-13 13:38:22 -0600

    Consumo de api JSONplaceholder, informacion mostrada en cards

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { ref } from "vue";
+import PostsCard from "./components/PostsCard.vue";
+
+const arrayPosts = ref([]);
+
+const getData = async () => {
+  try {
+    const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+    const data = await res.json();
+    arrayPosts.value = data;
+  } catch (error) {
+    console.log("Mamo esta cosa");
+  }
+};
+
+getData();
+</script>
+
+<template>
+  <div class="container">
+    <h2 class="d-flex justify-content-center p-2 text-success fw-bold">
+      Api json placeholder
+    </h2>
+    <PostsCard v-for="item in arrayPosts" :key="item.id" v-bind="item" />
+  </div>
+</template>
---
+++ src/components/PostsCard.vue
@@ -0,0 +1,17 @@
+<script setup>
+const props = defineProps({
+  id: Number,
+  title: String,
+  body: String,
+  userId: Number,
+});
+</script>
+
+<template>
+  <div class="card m-3">
+    <div class="body-card p-5">
+      <h5 class="card-title fw-bold">{{ id }} .- {{ title }}</h5>
+      <p>{{ body }}</p>
+    </div>
+  </div>
+</template>

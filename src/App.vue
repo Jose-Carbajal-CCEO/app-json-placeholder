f1c55027c059778cbaabb9216e3e490f1af3992d
Jose-Carbajal-CEEO <jose.carbajal@cceo.com.mx>
2024-11-13 14:59:07 -0600

    Boton agregar favoritos integrado

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,11 @@ import { ref } from "vue";
 import PostsCard from "./components/PostsCard.vue";
 
 const arrayPosts = ref([]);
+const favorite = ref("");
+
+const marcarFavorito = (title) => {
+  favorite.value = title;
+};
 
 const getData = async () => {
   try {
@@ -13,7 +18,6 @@ const getData = async () => {
     console.log("Mamo esta cosa");
   }
 };
-
 getData();
 </script>
 
@@ -22,6 +26,14 @@ getData();
     <h2 class="d-flex justify-content-center p-2 text-success fw-bold">
       Api json placeholder
     </h2>
-    <PostsCard v-for="item in arrayPosts" :key="item.id" v-bind="item" />
+    <h3>
+      Favorito: <span class="fst-italic">{{ favorite }}</span>
+    </h3>
+    <PostsCard
+      v-for="item in arrayPosts"
+      :key="item.id"
+      v-bind="item"
+      @newFavorite="marcarFavorito"
+    />
   </div>
 </template>
